10a7d7ef13aa3ef4d3f639b304e1b2d2e9981c8a
Vehiz <113064866+Vehiz@users.noreply.github.com>
2023-08-28 00:59:33 +0100

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,11 @@
 // import './App.css';
 import About from './pages/About/About'
-// import Footer from './components/Footer'
-// import Values from './components/Values';
-// import Team from './components/Team';
-// import Numbers from './components/Numbers'
 
 function App() {
   return (
-    <div>
+    <div className='wrapper'>
       <About/>
-      {/* <Numbers/> */}
-      {/* <Team/> */}
-      {/* <Values/> */}
-      {/* <Footer/> */}
+      
     </div>
   );
 }
